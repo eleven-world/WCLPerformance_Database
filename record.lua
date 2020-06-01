34eb748c38eda4744db5b93328346151254a516f
eleven <elevenworld0987@gmail.com>
2020-06-01 10:33:25 +0800

    2020-06-01 test2020-06-01 test2020-06-01 test2020-06-01 test2020-06-01 test2020-06-01 test2020-06-01 test2020-06-01 test2020-06-01 test2020-06-01 test2020-06-01 test2020-06-01 test2020-06-01 test2020-06-01 test2020-06-01 test2020-06-01 test2020-06-01 test2020-06-01 test2020-06-01 test2020-06-01 test

---
+++ record.lua
@@ -0,0 +1,1036 @@
+WCLRate_Database = {
+	["2020-06-01"] = {
+		dps = {
+			[2329] = {
+				[5] = {
+					[90] = {
+						Warlock = {
+							Affliction = {
+								amount = 7937286,
+								parses = 2131
+							},
+							Demonology = {
+								amount = 7463961,
+								parses = 1038
+							},
+							Destruction = {
+								amount = 6650500,
+								parses = 5414
+							}
+						},
+						Mage = {
+							Fire = {
+								amount = 7449214,
+								parses = 8477
+							},
+							Arcane = {
+								amount = 7148052,
+								parses = 547
+							},
+							Frost = {
+								amount = 6317722,
+								parses = 2296
+							}
+						},
+						Hunter = {
+							BeastMastery = {
+								amount = 7398098,
+								parses = 15123
+							},
+							Marksmanship = {
+								amount = 6652225,
+								parses = 406
+							},
+							Survival = {
+								amount = 6019698,
+								parses = 166
+							}
+						},
+						Warrior = {
+							Arms = {
+								amount = 7312600,
+								parses = 3509
+							},
+							Fury = {
+								amount = 6246047,
+								parses = 5006
+							},
+							Protection = {
+								amount = 3922750,
+								parses = 1869
+							}
+						},
+						Druid = {
+							Balance = {
+								amount = 7236440,
+								parses = 6459
+							},
+							Feral = {
+								amount = 6550320,
+								parses = 1179
+							},
+							Guardian = {
+								amount = 3591668,
+								parses = 1380
+							},
+							Restoration = {
+								amount = 746193,
+								parses = 4724
+							}
+						},
+						Shaman = {
+							Elemental = {
+								amount = 6947038,
+								parses = 2470
+							},
+							Enhancement = {
+								amount = 6101948,
+								parses = 1563
+							},
+							Restoration = {
+								amount = 1044922,
+								parses = 4065
+							}
+						},
+						DemonHunter = {
+							Havoc = {
+								amount = 6887730,
+								parses = 11013
+							},
+							Vengeance = {
+								amount = 3557920,
+								parses = 1549
+							}
+						},
+						Priest = {
+							Shadow = {
+								amount = 6886510,
+								parses = 3787
+							},
+							Discipline = {
+								amount = 2058715,
+								parses = 1614
+							},
+							Holy = {
+								amount = 852383,
+								parses = 3877
+							}
+						},
+						Rogue = {
+							Assassination = {
+								amount = 6658225,
+								parses = 4796
+							},
+							Subtlety = {
+								amount = 6621232,
+								parses = 67
+							},
+							Outlaw = {
+								amount = 5919915,
+								parses = 1544
+							}
+						},
+						Paladin = {
+							Retribution = {
+								amount = 6439360,
+								parses = 4479
+							},
+							Protection = {
+								amount = 3818832,
+								parses = 1811
+							},
+							Holy = {
+								amount = 1518874,
+								parses = 5983
+							}
+						},
+						DeathKnight = {
+							Unholy = {
+								amount = 6335386,
+								parses = 3180
+							},
+							Frost = {
+								amount = 6086580,
+								parses = 1119
+							},
+							Blood = {
+								amount = 4097615,
+								parses = 2840
+							}
+						},
+						Monk = {
+							Windwalker = {
+								amount = 5800970,
+								parses = 1997
+							},
+							Brewmaster = {
+								amount = 3800014,
+								parses = 2895
+							},
+							Mistweaver = {
+								amount = 935170,
+								parses = 2863
+							}
+						}
+					},
+					[50] = {
+						Warlock = {
+							Affliction = {
+								amount = 6117590,
+								parses = 2131
+							},
+							Demonology = {
+								amount = 5649580,
+								parses = 1038
+							},
+							Destruction = {
+								amount = 4958530,
+								parses = 5414
+							}
+						},
+						Druid = {
+							Balance = {
+								amount = 5726550,
+								parses = 6459
+							},
+							Feral = {
+								amount = 5041780,
+								parses = 1179
+							},
+							Guardian = {
+								amount = 2499725,
+								parses = 1380
+							},
+							Restoration = {
+								amount = 271276,
+								parses = 4724
+							}
+						},
+						Hunter = {
+							BeastMastery = {
+								amount = 5493950,
+								parses = 15123
+							},
+							Marksmanship = {
+								amount = 4957550,
+								parses = 406
+							},
+							Survival = {
+								amount = 4587385,
+								parses = 166
+							}
+						},
+						Mage = {
+							Fire = {
+								amount = 5465240,
+								parses = 8477
+							},
+							Arcane = {
+								amount = 5341140,
+								parses = 547
+							},
+							Frost = {
+								amount = 4836075,
+								parses = 2296
+							}
+						},
+						Shaman = {
+							Elemental = {
+								amount = 5389080,
+								parses = 2470
+							},
+							Enhancement = {
+								amount = 4660910,
+								parses = 1563
+							},
+							Restoration = {
+								amount = 343898,
+								parses = 4065
+							}
+						},
+						Warrior = {
+							Arms = {
+								amount = 5343200,
+								parses = 3509
+							},
+							Fury = {
+								amount = 4769500,
+								parses = 5006
+							},
+							Protection = {
+								amount = 2544580,
+								parses = 1869
+							}
+						},
+						Priest = {
+							Shadow = {
+								amount = 5215510,
+								parses = 3787
+							},
+							Discipline = {
+								amount = 1497280,
+								parses = 1614
+							},
+							Holy = {
+								amount = 372036,
+								parses = 3877
+							}
+						},
+						DemonHunter = {
+							Havoc = {
+								amount = 5110490,
+								parses = 11013
+							},
+							Vengeance = {
+								amount = 2294420,
+								parses = 1549
+							}
+						},
+						Rogue = {
+							Assassination = {
+								amount = 4976485,
+								parses = 4796
+							},
+							Subtlety = {
+								amount = 4581840,
+								parses = 67
+							},
+							Outlaw = {
+								amount = 4541995,
+								parses = 1544
+							}
+						},
+						DeathKnight = {
+							Unholy = {
+								amount = 4843270,
+								parses = 3180
+							},
+							Frost = {
+								amount = 4615440,
+								parses = 1119
+							},
+							Blood = {
+								amount = 2850040,
+								parses = 2840
+							}
+						},
+						Paladin = {
+							Retribution = {
+								amount = 4792000,
+								parses = 4479
+							},
+							Protection = {
+								amount = 2335760,
+								parses = 1811
+							},
+							Holy = {
+								amount = 850727,
+								parses = 5983
+							}
+						},
+						Monk = {
+							Windwalker = {
+								amount = 4421340,
+								parses = 1997
+							},
+							Brewmaster = {
+								amount = 2649140,
+								parses = 2895
+							},
+							Mistweaver = {
+								amount = 273777,
+								parses = 2863
+							}
+						}
+					},
+					[25] = {
+						Warlock = {
+							Affliction = {
+								amount = 5150940,
+								parses = 2131
+							},
+							Demonology = {
+								amount = 4566963,
+								parses = 1038
+							},
+							Destruction = {
+								amount = 4186738,
+								parses = 5414
+							}
+						},
+						Druid = {
+							Balance = {
+								amount = 4911130,
+								parses = 6459
+							},
+							Feral = {
+								amount = 4311960,
+								parses = 1179
+							},
+							Guardian = {
+								amount = 2086373,
+								parses = 1380
+							},
+							Restoration = {
+								amount = 137227,
+								parses = 4724
+							}
+						},
+						Hunter = {
+							BeastMastery = {
+								amount = 4671270,
+								parses = 15123
+							},
+							Marksmanship = {
+								amount = 4200110,
+								parses = 406
+							},
+							Survival = {
+								amount = 3963060,
+								parses = 166
+							}
+						},
+						Shaman = {
+							Elemental = {
+								amount = 4657270,
+								parses = 2470
+							},
+							Enhancement = {
+								amount = 3960480,
+								parses = 1563
+							},
+							Restoration = {
+								amount = 155634,
+								parses = 4065
+							}
+						},
+						Mage = {
+							Fire = {
+								amount = 4578780,
+								parses = 8477
+							},
+							Arcane = {
+								amount = 4440640,
+								parses = 547
+							},
+							Frost = {
+								amount = 4070543,
+								parses = 2296
+							}
+						},
+						Priest = {
+							Shadow = {
+								amount = 4420370,
+								parses = 3787
+							},
+							Discipline = {
+								amount = 1224558,
+								parses = 1614
+							},
+							Holy = {
+								amount = 213895,
+								parses = 3877
+							}
+						},
+						Warrior = {
+							Arms = {
+								amount = 4346010,
+								parses = 3509
+							},
+							Fury = {
+								amount = 3889005,
+								parses = 5006
+							},
+							Protection = {
+								amount = 2023690,
+								parses = 1869
+							}
+						},
+						DemonHunter = {
+							Havoc = {
+								amount = 4288270,
+								parses = 11013
+							},
+							Vengeance = {
+								amount = 1885815,
+								parses = 1549
+							}
+						},
+						Rogue = {
+							Assassination = {
+								amount = 4281320,
+								parses = 4796
+							},
+							Subtlety = {
+								amount = 3899470,
+								parses = 67
+							},
+							Outlaw = {
+								amount = 3834425,
+								parses = 1544
+							}
+						},
+						Paladin = {
+							Retribution = {
+								amount = 4012790,
+								parses = 4479
+							},
+							Protection = {
+								amount = 1921270,
+								parses = 1811
+							},
+							Holy = {
+								amount = 607092,
+								parses = 5983
+							}
+						},
+						DeathKnight = {
+							Unholy = {
+								amount = 3984978,
+								parses = 3180
+							},
+							Frost = {
+								amount = 3806040,
+								parses = 1119
+							},
+							Blood = {
+								amount = 2304635,
+								parses = 2840
+							}
+						},
+						Monk = {
+							Windwalker = {
+								amount = 3818720,
+								parses = 1997
+							},
+							Brewmaster = {
+								amount = 2141170,
+								parses = 2895
+							},
+							Mistweaver = {
+								amount = 145420,
+								parses = 2863
+							}
+						}
+					}
+				}
+			}
+		},
+		hps = {
+			[2329] = {
+				[5] = {
+					[90] = {
+						Paladin = {
+							Holy = {
+								amount = 9279820,
+								parses = 5983
+							},
+							Protection = {
+								amount = 3235218,
+								parses = 1811
+							},
+							Retribution = {
+								amount = 931382,
+								parses = 4479
+							}
+						},
+						Priest = {
+							Holy = {
+								amount = 9164058,
+								parses = 3877
+							},
+							Discipline = {
+								amount = 8269660,
+								parses = 1614
+							},
+							Shadow = {
+								amount = 1218168,
+								parses = 3787
+							}
+						},
+						Shaman = {
+							Restoration = {
+								amount = 8955544,
+								parses = 4065
+							},
+							Enhancement = {
+								amount = 771010,
+								parses = 1563
+							},
+							Elemental = {
+								amount = 596908,
+								parses = 2470
+							}
+						},
+						Druid = {
+							Restoration = {
+								amount = 8736690,
+								parses = 4724
+							},
+							Guardian = {
+								amount = 3250019,
+								parses = 1380
+							},
+							Feral = {
+								amount = 1065510,
+								parses = 1179
+							},
+							Balance = {
+								amount = 995041,
+								parses = 6459
+							}
+						},
+						Monk = {
+							Mistweaver = {
+								amount = 8377208,
+								parses = 2863
+							},
+							Brewmaster = {
+								amount = 2820996,
+								parses = 2895
+							},
+							Windwalker = {
+								amount = 703293,
+								parses = 1997
+							}
+						},
+						DeathKnight = {
+							Blood = {
+								amount = 3707346,
+								parses = 2840
+							},
+							Frost = {
+								amount = 755764,
+								parses = 1119
+							},
+							Unholy = {
+								amount = 683045,
+								parses = 3180
+							}
+						},
+						DemonHunter = {
+							Vengeance = {
+								amount = 2974090,
+								parses = 1549
+							},
+							Havoc = {
+								amount = 1148730,
+								parses = 11013
+							}
+						},
+						Warrior = {
+							Protection = {
+								amount = 2289410,
+								parses = 1869
+							},
+							Fury = {
+								amount = 894416,
+								parses = 5006
+							},
+							Arms = {
+								amount = 721541,
+								parses = 3509
+							}
+						},
+						Mage = {
+							Fire = {
+								amount = 1007472,
+								parses = 8477
+							},
+							Arcane = {
+								amount = 734695,
+								parses = 547
+							},
+							Frost = {
+								amount = 732995,
+								parses = 2296
+							}
+						},
+						Warlock = {
+							Destruction = {
+								amount = 983852,
+								parses = 5414
+							},
+							Demonology = {
+								amount = 962994,
+								parses = 1038
+							},
+							Affliction = {
+								amount = 925380,
+								parses = 2131
+							}
+						},
+						Hunter = {
+							Survival = {
+								amount = 788623,
+								parses = 166
+							},
+							BeastMastery = {
+								amount = 672655,
+								parses = 15123
+							},
+							Marksmanship = {
+								amount = 494752,
+								parses = 406
+							}
+						},
+						Rogue = {
+							Outlaw = {
+								amount = 774767,
+								parses = 1544
+							},
+							Subtlety = {
+								amount = 724214,
+								parses = 67
+							},
+							Assassination = {
+								amount = 684470,
+								parses = 4796
+							}
+						}
+					},
+					[50] = {
+						Priest = {
+							Holy = {
+								amount = 7001900,
+								parses = 3877
+							},
+							Discipline = {
+								amount = 5621170,
+								parses = 1614
+							},
+							Shadow = {
+								amount = 855776,
+								parses = 3787
+							}
+						},
+						Shaman = {
+							Restoration = {
+								amount = 6731150,
+								parses = 4065
+							},
+							Enhancement = {
+								amount = 511407,
+								parses = 1563
+							},
+							Elemental = {
+								amount = 393065,
+								parses = 2470
+							}
+						},
+						Paladin = {
+							Holy = {
+								amount = 6706080,
+								parses = 5983
+							},
+							Protection = {
+								amount = 2391990,
+								parses = 1811
+							},
+							Retribution = {
+								amount = 628641,
+								parses = 4479
+							}
+						},
+						Druid = {
+							Restoration = {
+								amount = 6661355,
+								parses = 4724
+							},
+							Guardian = {
+								amount = 2442415,
+								parses = 1380
+							},
+							Feral = {
+								amount = 738588,
+								parses = 1179
+							},
+							Balance = {
+								amount = 633247,
+								parses = 6459
+							}
+						},
+						Monk = {
+							Mistweaver = {
+								amount = 6523900,
+								parses = 2863
+							},
+							Brewmaster = {
+								amount = 2156530,
+								parses = 2895
+							},
+							Windwalker = {
+								amount = 475943,
+								parses = 1997
+							}
+						},
+						DeathKnight = {
+							Blood = {
+								amount = 2820400,
+								parses = 2840
+							},
+							Frost = {
+								amount = 509424,
+								parses = 1119
+							},
+							Unholy = {
+								amount = 460693,
+								parses = 3180
+							}
+						},
+						DemonHunter = {
+							Vengeance = {
+								amount = 2171170,
+								parses = 1549
+							},
+							Havoc = {
+								amount = 728526,
+								parses = 11013
+							}
+						},
+						Warrior = {
+							Protection = {
+								amount = 1674020,
+								parses = 1869
+							},
+							Fury = {
+								amount = 634314,
+								parses = 5006
+							},
+							Arms = {
+								amount = 408560,
+								parses = 3509
+							}
+						},
+						Warlock = {
+							Destruction = {
+								amount = 719858,
+								parses = 5414
+							},
+							Affliction = {
+								amount = 715894,
+								parses = 2131
+							},
+							Demonology = {
+								amount = 698846,
+								parses = 1038
+							}
+						},
+						Mage = {
+							Fire = {
+								amount = 690437,
+								parses = 8477
+							},
+							Arcane = {
+								amount = 550364,
+								parses = 547
+							},
+							Frost = {
+								amount = 533198,
+								parses = 2296
+							}
+						},
+						Hunter = {
+							Survival = {
+								amount = 585171,
+								parses = 166
+							},
+							BeastMastery = {
+								amount = 435778,
+								parses = 15123
+							},
+							Marksmanship = {
+								amount = 310336,
+								parses = 406
+							}
+						},
+						Rogue = {
+							Outlaw = {
+								amount = 524560,
+								parses = 1544
+							},
+							Subtlety = {
+								amount = 513065,
+								parses = 67
+							},
+							Assassination = {
+								amount = 474087,
+								parses = 4796
+							}
+						}
+					},
+					[25] = {
+						Priest = {
+							Holy = {
+								amount = 6006170,
+								parses = 3877
+							},
+							Discipline = {
+								amount = 4555955,
+								parses = 1614
+							},
+							Shadow = {
+								amount = 684811,
+								parses = 3787
+							}
+						},
+						Druid = {
+							Restoration = {
+								amount = 5768665,
+								parses = 4724
+							},
+							Guardian = {
+								amount = 2075530,
+								parses = 1380
+							},
+							Feral = {
+								amount = 579336,
+								parses = 1179
+							},
+							Balance = {
+								amount = 466906,
+								parses = 6459
+							}
+						},
+						Shaman = {
+							Restoration = {
+								amount = 5767030,
+								parses = 4065
+							},
+							Enhancement = {
+								amount = 379899,
+								parses = 1563
+							},
+							Elemental = {
+								amount = 305103,
+								parses = 2470
+							}
+						},
+						Monk = {
+							Mistweaver = {
+								amount = 5670600,
+								parses = 2863
+							},
+							Brewmaster = {
+								amount = 1838370,
+								parses = 2895
+							},
+							Windwalker = {
+								amount = 358392,
+								parses = 1997
+							}
+						},
+						Paladin = {
+							Holy = {
+								amount = 5569820,
+								parses = 5983
+							},
+							Protection = {
+								amount = 1990690,
+								parses = 1811
+							},
+							Retribution = {
+								amount = 489654,
+								parses = 4479
+							}
+						},
+						DeathKnight = {
+							Blood = {
+								amount = 2422225,
+								parses = 2840
+							},
+							Frost = {
+								amount = 378297,
+								parses = 1119
+							},
+							Unholy = {
+								amount = 341155,
+								parses = 3180
+							}
+						},
+						DemonHunter = {
+							Vengeance = {
+								amount = 1807480,
+								parses = 1549
+							},
+							Havoc = {
+								amount = 567301,
+								parses = 11013
+							}
+						},
+						Warrior = {
+							Protection = {
+								amount = 1375465,
+								parses = 1869
+							},
+							Fury = {
+								amount = 498514,
+								parses = 5006
+							},
+							Arms = {
+								amount = 300631,
+								parses = 3509
+							}
+						},
+						Warlock = {
+							Destruction = {
+								amount = 599937,
+								parses = 5414
+							},
+							Affliction = {
+								amount = 593198,
+								parses = 2131
+							},
+							Demonology = {
+								amount = 591443,
+								parses = 1038
+							}
+						},
+						Mage = {
+							Fire = {
+								amount = 570393,
+								parses = 8477
+							},
+							Arcane = {
+								amount = 443013,
+								parses = 547
+							},
+							Frost = {
+								amount = 421007,
+								parses = 2296
+							}
+						},
+						Hunter = {
+							Survival = {
+								amount = 448297,
+								parses = 166
+							},
+							BeastMastery = {
+								amount = 333391,
+								parses = 15123
+							},
+							Marksmanship = {
+								amount = 233778,
+								parses = 406
+							}
+						},
+						Rogue = {
+							Outlaw = {
+								amount = 409454,
+								parses = 1544
+							},
+							Subtlety = {
+								amount = 399459,
+								parses = 67
+							},
+							Assassination = {
+								amount = 375963,
+								parses = 4796
+							}
+						}
+					}
+				}
+			}
+		}
+	}
+}
